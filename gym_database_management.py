8d616c2e3160e040e019b01834ba89447d7093f4
b-smith94 <byrondsmith9@gmail.com>
2024-09-19 12:34:07 -0400

    completed add_member function

--- gym_database_management.py
+++ gym_database_management.py
@@ -1,8 +1,24 @@
-import connect_mysql
+from connect_mysql import connect_database
 
 #Task 1
-def add_member(id, name, age):
-    pass
+def add_member(name, age):
+    conn = connect_database()
+    if conn is not None:
+        try:
+            cursor = conn.cursor()
+            new_member = (name, age)
+            query = "INSERT INTO Members (name, age) VALUES (%s, %s)"
+            cursor.execute(query, new_member)
+            conn.commit()
+
+            print("New member added successfully!")
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+
+
 
 #Task 2
 def add_workout_session(member_id, date, duration_minutes, calories_burned):
@@ -15,3 +31,5 @@ def update_member_age(member_id, new_age):
 #Task 4
 def delete_workout_session(session_id):
     pass
+
+add_member()
