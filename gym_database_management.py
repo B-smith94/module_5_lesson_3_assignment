3bec065efe1e8a1093a4bab4a61f42713b7e077e
b-smith94 <byrondsmith9@gmail.com>
2024-09-19 12:41:17 -0400

    Began work on delete_workout_session function

--- gym_database_management.py
+++ gym_database_management.py
@@ -18,8 +18,6 @@ def add_member(name, age):
             cursor.close()
             conn.close()
 
-
-
 #Task 2
 def add_workout_session(member_id, date, duration_minutes, calories_burned):
     pass
@@ -30,6 +28,23 @@ def update_member_age(member_id, new_age):
 
 #Task 4
 def delete_workout_session(session_id):
-    pass
+    conn = connect_database()
+    if conn is not None:
+        try:
+            cursor = conn.cursor()
+            query = "DELETE FROM WorkoutSessions WHERE session_id = %s"
+            
+            final_decision = input("Are you sure you want to delete this session? (yes/no) WARNING: This action is irreversable: ")
+            if final_decision.lower() == 'yes':
+                cursor.execute(query, session_id)
+                print("Session deleted successfully.")
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            cursor.close()
+            conn.close()
 
 add_member()
+add_workout_session()
+update_member_age()
+delete_workout_session()
